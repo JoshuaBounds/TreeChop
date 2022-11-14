9a011d942166eda1d799ee05d2bfb1ef65f2d519
Josh <jbounder@gmail.com>
2022-11-13 16:52:30 -0800

    checkpoint

---
+++ Assets/_TreeChop/Scripts/FollowPlayer.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform playerUnit;
+    public Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //get the players position and add it with offset, then store it to transform.position aka the cameras position
+        transform.position = playerUnit.position + offset;
+    }
+}
